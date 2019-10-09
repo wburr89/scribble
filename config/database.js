4ef1be1f364661c720c25ed93e8d020f5106e388
wburr89 <wburr89@gmail.com>
2019-10-09 09:59:15 -0700

    Encoded special characters in connection password

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://wburr89@gmail.com:Laylita21!@scribble-o5a4v.mongodb.net/admin?retryWrites=true&w=majority"
+      "mongodb+srv://wburr89:Laylita21%21@scribble-o5a4v.mongodb.net/test?retryWrites=true&w=majority"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/scrbble-dev" };
