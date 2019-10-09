d7be06a0793bdf2c6b29c36338ddbaf9c438246c
wburr89 <wburr89@gmail.com>
2019-10-08 20:36:42 -0700

    Updated username

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = {
     mongoURI:
-      "mongodb+srv://wburr89:Jan141989!@scribble-o5a4v.mongodb.net/admin?retryWrites=true&w=majority"
+      "mongodb+srv://wburr89@gmail.com:Jan141989!@scribble-o5a4v.mongodb.net/admin?retryWrites=true&w=majority"
   };
 } else {
   module.exports = { mongoURI: "mongodb://localhost/scrbble-dev" };
